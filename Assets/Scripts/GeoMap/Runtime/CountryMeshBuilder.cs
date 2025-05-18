0b13d7ac190279fcc51ae6f55f788d5cf8de9fba
Sandro Ponticelli <s.ponticelli@whatwapp.com>
2025-05-18 17:27:04 +0200

    refactor: Improve variable naming in CountryMeshBuilder for better readability

--- Assets/Scripts/GeoMap/Runtime/CountryMeshBuilder.cs
+++ Assets/Scripts/GeoMap/Runtime/CountryMeshBuilder.cs
@@ -27,7 +27,7 @@ namespace GeoMap
 
             // Create parent GameObjects for MultiPolygon types
             GameObject borderParent = null;
-            GameObject surfaceParent2 = null;
+            GameObject surfaceLayerParent = null;
 
             if (geometryType == "MultiPolygon")
             {
@@ -38,80 +38,80 @@ namespace GeoMap
                 // Create parent for surface meshes if needed
                 if (createSurface)
                 {
-                    surfaceParent2 = new GameObject(countryName + " layer");
-                    surfaceParent2.transform.SetParent(surfaceParent, false);
+                    surfaceLayerParent = new GameObject(countryName + " layer");
+                    surfaceLayerParent.transform.SetParent(surfaceParent, false);
                 }
             }
 
             // Process each polygon in the coordinates
             int polygonIndex = 0;
-            foreach (var bb in country["geometry"]["coordinates"].list)
+            foreach (var coordinateSet in country["geometry"]["coordinates"].list)
             {
-                var jo = ExtractCoordinates(bb);
-                if (jo.list == null) continue;
+                var processedCoordinates = ExtractCoordinates(coordinateSet);
+                if (processedCoordinates.list == null) continue;
 
-                var count = jo.list.Count;
-                if (count < 3) continue;
+                var vertexCount = processedCoordinates.list.Count;
+                if (vertexCount < 3) continue;
 
-                List<Vector3> earthBoundarEnds = new List<Vector3>();
-                earthBoundarEnds.Clear();
+                List<Vector3> boundaryVertices = new List<Vector3>();
+                boundaryVertices.Clear();
 
-                for (int i = 0; i < count; i++)
+                for (int i = 0; i < vertexCount; i++)
                 {
-                    var c = jo.list[i];
-                    var dotMerc = GeoConvert.LatLonToMetersForEarth(c[1].f, c[0].f);
-                    dotMerc += new Vector3d(center.x, center.y, center.z);
-                    earthBoundarEnds.Add((Vector3)dotMerc);
+                    var coordinate = processedCoordinates.list[i];
+                    var worldPosition = GeoConvert.LatLonToMetersForEarth(coordinate[1].f, coordinate[0].f);
+                    worldPosition += new Vector3d(center.x, center.y, center.z);
+                    boundaryVertices.Add((Vector3)worldPosition);
                 }
 
                 // Build border mesh with appropriate parent
                 Transform borderParentTransform = (geometryType == "MultiPolygon") ? borderParent.transform : outlineParent;
                 string borderName = (geometryType == "MultiPolygon") ? $"Part_{polygonIndex}" : countryName;
-                var borderMeshContainer = BuildBorderMesh(borderName, borderParentTransform, earthBoundarEnds);
+                var borderMeshContainer = BuildBorderMesh(borderName, borderParentTransform, boundaryVertices);
 
                 // Build surface mesh with appropriate parent if needed
                 if (createSurface)
                 {
-                    Transform surfaceParentTransform = (geometryType == "MultiPolygon") ? surfaceParent2.transform : surfaceParent;
+                    Transform surfaceParentTransform = (geometryType == "MultiPolygon") ? surfaceLayerParent.transform : surfaceParent;
                     string surfaceName = (geometryType == "MultiPolygon") ? $"Part_{polygonIndex}" : countryName;
-                    BuildSurfaceMesh(surfaceName, surfaceParentTransform, earthBoundarEnds, geometryType == "Polygon");
+                    BuildSurfaceMesh(surfaceName, surfaceParentTransform, boundaryVertices, geometryType == "Polygon");
                 }
 
                 polygonIndex++;
             }
         }
 
-        private void BuildSurfaceMesh(string name, Transform parentTransform, List<Vector3> earthBoundarEnds, bool createLayer = true)
+        private void BuildSurfaceMesh(string name, Transform parentTransform, List<Vector3> boundaryVertices, bool createLayer = true)
         {
-            var earthMeshBoundary2 = new GameObject("mesh").AddComponent<MeshFilter>();
-            earthMeshBoundary2.gameObject.AddComponent<MeshRenderer>();
-            var earthMesh2 = earthMeshBoundary2.mesh;
-            var points = earthBoundarEnds;
-            var md2 = new MeshData();
-            var inp = new MeshInputData(points.Count);
-            for (int d = 0; d < points.Count; d++)
+            var surfaceMeshObject = new GameObject("mesh").AddComponent<MeshFilter>();
+            surfaceMeshObject.gameObject.AddComponent<MeshRenderer>();
+            var surfaceMesh = surfaceMeshObject.mesh;
+            var points = boundaryVertices;
+            var meshData = new MeshData();
+            var meshInput = new MeshInputData(points.Count);
+            for (int vertexIndex = 0; vertexIndex < points.Count; vertexIndex++)
             {
-                inp.AddPoint(points[d].x, points[d].y);
-                inp.AddSegment(d, (d + 1) % points.Count);
+                meshInput.AddPoint(points[vertexIndex].x, points[vertexIndex].y);
+                meshInput.AddSegment(vertexIndex, (vertexIndex + 1) % points.Count);
             }
 
-            CreateMeshForPolygon(inp, md2, points, false);
+            CreateMeshForPolygon(meshInput, meshData, points, false);
 
-            earthMesh2.vertices = md2.Vertices.ToArray();
-            earthMesh2.triangles = md2.Indices.ToArray();
-            earthMesh2.SetUVs(0, md2.UV);
-            earthMesh2.RecalculateNormals();
+            surfaceMesh.vertices = meshData.Vertices.ToArray();
+            surfaceMesh.triangles = meshData.Indices.ToArray();
+            surfaceMesh.SetUVs(0, meshData.UV);
+            surfaceMesh.RecalculateNormals();
 
-            earthMeshBoundary2.GetComponent<MeshRenderer>().material = surfaceMaterial;
-            earthMeshBoundary2.transform.name = name;
+            surfaceMeshObject.GetComponent<MeshRenderer>().material = surfaceMaterial;
+            surfaceMeshObject.transform.name = name;
 
             if (name.Contains("Lesotho"))
-                earthMeshBoundary2.transform.localPosition -= new Vector3(0, 0, 1);
+                surfaceMeshObject.transform.localPosition -= new Vector3(0, 0, 1);
 
             // Set parent directly if we're not creating a layer (for MultiPolygon parts)
             if (!createLayer)
             {
-                earthMeshBoundary2.transform.SetParent(parentTransform, false);
+                surfaceMeshObject.transform.SetParent(parentTransform, false);
             }
             else
             {
@@ -129,39 +129,39 @@ namespace GeoMap
 
                 if (containsLayer)
                 {
-                    earthMeshBoundary2.transform.parent = parentLayer.transform;
+                    surfaceMeshObject.transform.parent = parentLayer.transform;
                 }
                 else
                 {
                     parentLayer = new GameObject(name + " layer");
                     parentLayer.transform.parent = parentTransform;
-                    earthMeshBoundary2.transform.parent = parentLayer.transform;
+                    surfaceMeshObject.transform.parent = parentLayer.transform;
                 }
             }
 
-            earthMeshBoundary2.gameObject.AddComponent<MeshCollider>();
+            surfaceMeshObject.gameObject.AddComponent<MeshCollider>();
         }
 
-        private MeshFilter BuildBorderMesh(string name, Transform parentTransform, List<Vector3> earthBoundarEnds)
+        private MeshFilter BuildBorderMesh(string name, Transform parentTransform, List<Vector3> boundaryVertices)
         {
-            var meshContainer = new GameObject("mesh").AddComponent<MeshFilter>();
-            meshContainer.gameObject.AddComponent<MeshRenderer>();
-            var countryMesh = meshContainer.mesh;
+            var borderMeshObject = new GameObject("mesh").AddComponent<MeshFilter>();
+            borderMeshObject.gameObject.AddComponent<MeshRenderer>();
+            var borderMesh = borderMeshObject.mesh;
 
-            var md = new MeshData();
+            var meshData = new MeshData();
 
-            CreateMeshForLinestring(earthBoundarEnds, md, borderWidth);
-            countryMesh.vertices = md.Vertices.ToArray();
-            countryMesh.triangles = md.Indices.ToArray();
-            countryMesh.SetUVs(0, md.UV);
-            countryMesh.RecalculateNormals();
+            CreateMeshForLinestring(boundaryVertices, meshData, borderWidth);
+            borderMesh.vertices = meshData.Vertices.ToArray();
+            borderMesh.triangles = meshData.Indices.ToArray();
+            borderMesh.SetUVs(0, meshData.UV);
+            borderMesh.RecalculateNormals();
 
-            meshContainer.name = name;
+            borderMeshObject.name = name;
 
-            meshContainer.GetComponent<MeshRenderer>().material = borderMaterial;
-            meshContainer.transform.SetParent(parentTransform, false);
+            borderMeshObject.GetComponent<MeshRenderer>().material = borderMaterial;
+            borderMeshObject.transform.SetParent(parentTransform, false);
 
-            return meshContainer;
+            return borderMeshObject;
         }
 
         private string GetCountryName(JsonNode country)
@@ -173,122 +173,113 @@ namespace GeoMap
             return "Unknown";
         }
 
-        private static JsonNode ExtractCoordinates(JsonNode bb)
+        private static JsonNode ExtractCoordinates(JsonNode coordinateNode)
         {
-            JsonNode jo = null;
-            if (bb.list == null)
+            JsonNode processedNode = null;
+            if (coordinateNode.list == null)
             {
-                //print("-1");
-                jo = bb;
+                processedNode = coordinateNode;
             }
-            else if (bb.list.Count == 0)
+            else if (coordinateNode.list.Count == 0)
             {
-                //print("-1.0");
-                jo = bb;
+                processedNode = coordinateNode;
             }
-            else if (bb.list[0].list == null)
+            else if (coordinateNode.list[0].list == null)
             {
-                //print("-2");
-                jo = bb;
+                processedNode = coordinateNode;
             }
-            else if (bb.list[0].list.Count == 0)
+            else if (coordinateNode.list[0].list.Count == 0)
             {
-                //print("-2.0");
-                jo = bb;
+                processedNode = coordinateNode;
             }
-            else jo = (bb.list[0].list[0].IsArray) ? bb.list[0] : bb;
+            else processedNode = (coordinateNode.list[0].list[0].IsArray) ? coordinateNode.list[0] : coordinateNode;
 
-            return jo;
+            return processedNode;
         }
 
-        private void CreateMeshForPolygon(MeshInputData corners, MeshData meshdata, List<Vector3> ends, bool temp)
+        private void CreateMeshForPolygon(MeshInputData corners, MeshData meshData, List<Vector3> vertices, bool reverseWinding)
         {
-            var mesh = new TriangularMesh();
-            mesh.Behavior.Algorithm = TriangulationAlgorithm.SweepLine;
-            mesh.Behavior.Quality = true;
-            mesh.Triangulate(corners);
-            var vertsStartCount = meshdata.Vertices.Count;
-            meshdata.Vertices = ends;/*AddRange(corners.Points.Select(x => new Vector3((float)x.X, (float)x.Y, 0)).ToList());*/
-            foreach (var tri in mesh.Triangles)
+            var triangularMesh = new TriangularMesh();
+            triangularMesh.Behavior.Algorithm = TriangulationAlgorithm.SweepLine;
+            triangularMesh.Behavior.Quality = true;
+            triangularMesh.Triangulate(corners);
+            var verticesStartIndex = meshData.Vertices.Count;
+            meshData.Vertices = vertices;
+            foreach (var triangle in triangularMesh.Triangles)
             {
-                if ((tri.P0 >= corners.Count) || (tri.P1 >= corners.Count) || (tri.P2 >= corners.Count)) continue;
+                if ((triangle.P0 >= corners.Count) || (triangle.P1 >= corners.Count) || (triangle.P2 >= corners.Count)) continue;
 
-                if (!temp)
+                if (!reverseWinding)
                 {
-                    meshdata.Indices.Add(vertsStartCount + tri.P1);//1
-                    meshdata.Indices.Add(vertsStartCount + tri.P0);//0
-                    meshdata.Indices.Add(vertsStartCount + tri.P2);//2
+                    meshData.Indices.Add(verticesStartIndex + triangle.P1);
+                    meshData.Indices.Add(verticesStartIndex + triangle.P0);
+                    meshData.Indices.Add(verticesStartIndex + triangle.P2);
                 }
                 else
                 {
-                    meshdata.Indices.Add(vertsStartCount + tri.P0);//1
-                    meshdata.Indices.Add(vertsStartCount + tri.P1);//0
-                    meshdata.Indices.Add(vertsStartCount + tri.P2);//2
+                    meshData.Indices.Add(verticesStartIndex + triangle.P0);
+                    meshData.Indices.Add(verticesStartIndex + triangle.P1);
+                    meshData.Indices.Add(verticesStartIndex + triangle.P2);
                 }
             }
         }
 
 
-        private void CreateMeshForLinestring(List<Vector3> list, MeshData md, float width)
+        private void CreateMeshForLinestring(List<Vector3> vertices, MeshData meshData, float width)
         {
-            var vertsStartCount = md.Vertices.Count;
-            Vector3 lastPos = Vector3.zero;
-            var norm = Vector3.zero;
-            for (int i = 1; i < list.Count; i++)
+            var verticesStartIndex = meshData.Vertices.Count;
+            Vector3 lastPosition = Vector3.zero;
+            var normal = Vector3.zero;
+            for (int i = 1; i < vertices.Count; i++)
             {
-                var p1 = list[i - 1];
-                var p2 = list[i];
-                var p3 = p2;
-                if (i + 1 < list.Count)
-                    p3 = list[i + 1];
-                if (lastPos == Vector3.zero)
+                var previousPoint = vertices[i - 1];
+                var currentPoint = vertices[i];
+                var nextPoint = currentPoint;
+                if (i + 1 < vertices.Count)
+                    nextPoint = vertices[i + 1];
+                if (lastPosition == Vector3.zero)
                 {
-                    lastPos = Vector3.Lerp(p1, p2, 0f);
-                    norm = GetNormal(p1, lastPos, p2) * width; // 0.0025f;
-                    md.Vertices.Add(lastPos + norm);
-                    md.Vertices.Add(lastPos - norm);
+                    lastPosition = Vector3.Lerp(previousPoint, currentPoint, 0f);
+                    normal = GetNormal(previousPoint, lastPosition, currentPoint) * width;
+                    meshData.Vertices.Add(lastPosition + normal);
+                    meshData.Vertices.Add(lastPosition - normal);
                 }
 
-                lastPos = Vector3.Lerp(p1, p2, 1f); //p2
-                norm = GetNormal(p1, lastPos, p3) * width; // 0.0025f;
-                md.Vertices.Add(lastPos + norm);
-                md.Vertices.Add(lastPos - norm);
+                lastPosition = Vector3.Lerp(previousPoint, currentPoint, 1f); // currentPoint
+                normal = GetNormal(previousPoint, lastPosition, nextPoint) * width;
+                meshData.Vertices.Add(lastPosition + normal);
+                meshData.Vertices.Add(lastPosition - normal);
             }
 
-            for (int j = vertsStartCount; j <= md.Vertices.Count - 3; j += 2)
+            for (int vertexIndex = verticesStartIndex; vertexIndex <= meshData.Vertices.Count - 3; vertexIndex += 2)
             {
-                {
-                    md.Indices.Add(j);
-                    md.Indices.Add(j + 2);
-                    md.Indices.Add(j + 1);
-                    md.Indices.Add(j + 1);
-                    md.Indices.Add(j + 2);
-                    md.Indices.Add(j + 3);
-                }
+                meshData.Indices.Add(vertexIndex);
+                meshData.Indices.Add(vertexIndex + 2);
+                meshData.Indices.Add(vertexIndex + 1);
+                meshData.Indices.Add(vertexIndex + 1);
+                meshData.Indices.Add(vertexIndex + 2);
+                meshData.Indices.Add(vertexIndex + 3);
             }
         }
 
-        private Vector3 GetNormal(Vector3 p1, Vector3 newPos, Vector3 p2)
+        private Vector3 GetNormal(Vector3 previousPoint, Vector3 currentPoint, Vector3 nextPoint)
         {
-            if (newPos == p1 || newPos == p2)
+            if (currentPoint == previousPoint || currentPoint == nextPoint)
             {
-                var n = (p2 - p1).normalized;
-                //return new Vector3(-n.z, 0, n.x);
-                return new Vector3(-n.y, n.x, 0);
+                var direction = (nextPoint - previousPoint).normalized;
+                return new Vector3(-direction.y, direction.x, 0);
             }
 
-            var b = (p2 - newPos).normalized + newPos;
-            var a = (p1 - newPos).normalized + newPos;
-            var t = (b - a).normalized;
-            if (t == Vector3.zero)
+            var vectorToNext = (nextPoint - currentPoint).normalized + currentPoint;
+            var vectorToPrevious = (previousPoint - currentPoint).normalized + currentPoint;
+            var tangent = (vectorToNext - vectorToPrevious).normalized;
+            if (tangent == Vector3.zero)
             {
-                var n = (p2 - p1).normalized;
-                //return new Vector3(-n.z, 0, n.x);
-                return new Vector3(-n.y, n.x, 0);
+                var direction = (nextPoint - previousPoint).normalized;
+                return new Vector3(-direction.y, direction.x, 0);
             }
 
-            //return new Vector3(-t.z, 0, t.x);
-            return new Vector3(-t.y, t.x, 0);
+            return new Vector3(-tangent.y, tangent.x, 0);
         }
     }
 }
